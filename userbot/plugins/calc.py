60cc670289addd2e2410a0a73c4366a3a6866bf0
Black hat hacker <95142447+Kajukatliii@users.noreply.github.com>
2022-03-08 06:48:08 -0800

    Update calc.py

--- userbot/plugins/calc.py
+++ userbot/plugins/calc.py
@@ -1,5 +1,3 @@
-# For UniBorg
-# By Priyam Kalra
 # Syntax (.calc <term1><operator><term2>)
 # For eg .calc 02*02 or 99*99 (the zeros are important) (two terms and two digits max)
 from telethon import events
